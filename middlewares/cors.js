7cd38788649f97148777020ada68ae2d57aa4b1d
Devid Fan <cptdavid@yandex.ru>
2023-11-06 13:37:18 +0300

    DiplomFinal

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,9 +3,9 @@ const allowedCors = [
   'http://davidmovie.nomoredomainsmonster.ru',
   'https://api.davidmovie.nomoredomainsicu.ru/users/me',
   'https://api.davidmovie.nomoredomainsicu.ru/signup',
-  'http://localhost:3000/',
-  'http://localhost:3001/',
-  'http://localhost:4000/',
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'http://localhost:4000',
   'https://158.160.80.64',
   'http://158.160.80.64',
 ];
@@ -29,3 +29,68 @@ module.exports = (req, res, next) => {
 
   return next();
 };
+
+// const allowedCors = [
+//   'https://davidmovie.nomoredomainsmonster.ru',
+//   'http://davidmovie.nomoredomainsmonster.ru',
+//   'https://api.davidmovie.nomoredomainsicu.ru/users/me',
+//   'https://api.davidmovie.nomoredomainsicu.ru/signup',
+//   'http://localhost:3000/',
+//   'http://localhost:3001/',
+//   'http://localhost:4000/',
+//   'https://158.160.80.64',
+//   'http://158.160.80.64',
+// ];
+
+// module.exports = (req, res, next) => {
+//   const { origin } = req.headers;
+//   const { method } = req;
+//   const requestHeaders = req.headers['access-control-request-headers'];
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//     res.header('Access-Control-Allow-Credentials', true);
+//   }
+//   if (method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+
+//     return res.end();
+//   }
+
+//   return next();
+// };
+
+// const allowedCors = [
+//   'https://davidthebest.nomoredomainsicu.ru',
+//   'http://davidthebest.nomoredomainsicu.ru',
+//   'https://api.davidthebest.nomoredomainsicu.ru/users/me',
+//   'https://api.davidthebest.nomoredomainsicu.ru/cards',
+//   'https://api.davidthebest.nomoredomainsicu.ru/signup',
+//   'http://localhost:3000',
+//   'http://localhost:3001',
+//   'http://localhost:4000',
+//   'https://158.160.72.189',
+//   'http://158.160.72.189',
+// ];
+
+// module.exports = (req, res, next) => {
+//   const { origin } = req.headers;
+//   const { method } = req;
+//   const requestHeaders = req.headers['access-control-request-headers'];
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//     res.header('Access-Control-Allow-Credentials', true);
+//   }
+//   if (method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+
+//     return res.end();
+//   }
+
+//   return next();
+// };
